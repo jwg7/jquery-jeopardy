0680017bde029ceceabf12d096baa3c59d5eee93
John G <johngouldman@pm.me>
2022-08-02 12:22:54 -0500

    wireframe completed

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ scoreLabel.appendChild(score);
 
 //create grid
 let grid = document.createElement("div");
-// create grid class
+// create grid id
 grid.id = "grid";
 // append grid to document
 document.body.appendChild(grid);
@@ -58,7 +58,6 @@ grid.style.gridTemplateColumns = "auto auto auto auto auto";
 /////////////////////////////
 // add cells to grid container
 /////////////////////////////
-// resource used: https://code-boxx.com/create-grid-javascript/#sec-grid
 
 // create container
 let container = document.querySelector("#grid");
@@ -67,11 +66,16 @@ let container = document.querySelector("#grid");
 // $100 cells
 //////////////////////
 
+
+
 function create100() {
   for (let i = 1; i <= 5; i++) {
     let cell100 = document.createElement("div");
+    // set same class for all cells
+    cell100.className = 'cell'
     cell100.innerHTML = "$100";
     container.appendChild(cell100);
+    
   }
 }
 
@@ -84,6 +88,8 @@ create100();
 function create200() {
   for (let i = 1; i <= 5; i++) {
     let cell200 = document.createElement("div");
+    // set same class for all cells
+    cell200.className = 'cell'
     cell200.innerHTML = "$200";
     container.appendChild(cell200);
   }
@@ -96,50 +102,92 @@ create200();
 //////////////////
 
 function create300() {
-    for (let i = 1; i <= 5; i++) {
-      let cell300 = document.createElement("div");
-      cell300.innerHTML = "$300";
-      container.appendChild(cell300);
-    }
+  for (let i = 1; i <= 5; i++) {
+    let cell300 = document.createElement("div");
+    // set same class for all cells
+    cell300.className = 'cell'
+    cell300.innerHTML = "$300";
+    container.appendChild(cell300);
   }
+}
 
-  create300()
+create300();
 
-  //////////////////
+//////////////////
 // $400 cells
 //////////////////
 
 function create400() {
-    for (let i = 1; i <= 5; i++) {
-      let cell400 = document.createElement("div");
-      cell400.innerHTML = "$400";
-      container.appendChild(cell400);
-    }
+  for (let i = 1; i <= 5; i++) {
+    let cell400 = document.createElement("div");
+    // set same class for all cells
+    cell400.className = 'cell'
+    cell400.innerHTML = "$400";
+    container.appendChild(cell400);
   }
+}
 
-  create400();
+create400();
 
-  //////////////////
+//////////////////
 // $500 cells
 //////////////////
 
 function create500() {
-    for (let i = 1; i <= 5; i++) {
-      let cell500 = document.createElement("div");
-      cell500.innerHTML = "$500";
-      container.appendChild(cell500);
-    }
+  for (let i = 1; i <= 5; i++) {
+    let cell500 = document.createElement("div");
+    // set same class for all cells
+    cell500.className = 'cell'
+    cell500.innerHTML = "$500";
+    container.appendChild(cell500);
   }
+}
 
-  create500();
+create500();
 
 /////////////////////////////////////
 // place where user sees question prompt
 //////////////////////////////////////
+let section = document.createElement('section');
+let question = document.createElement('p')
+question.innerText = 'SELECT A BOX TO START'
+question.style.fontSize = '20px'
+question.style.textAlign = 'center'
+document.body.appendChild(section)
+section.appendChild(question)
 
-
+////////////////////////////////
+// create HTML break. 
+// Need spacing between grid, question, and answer
+/////////////////////////////////////
+// let br = document.createElement('br')
+// document.body.appendChild(br)
 
 
 /////////////////////////////////
 // place where user submits answer
 ///////////////////////////////////
+
+// create form. need input. need submit button.
+let form = document.createElement('form')
+form.id = 'form'
+document.body.appendChild(form)
+let input = document.createElement('input')
+input.placeholder = 'Enter your answer'
+input.type = 'text'
+input.id = 'input'
+input.style.width = '250px'
+input.style.height = '40px'
+form.appendChild(input)
+let button = document.createElement('button')
+button.type = 'submit'
+button.innerText = "SUBMIT"
+button.style.height = '46px'
+button.id = 'submit'
+form.appendChild(button)
+// css for form
+form.style.textAlign = 'center'
+
+
+
+
