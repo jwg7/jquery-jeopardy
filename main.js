66e20c83fab9e411d630f679d239898f27c59c92
John G <johngouldman@pm.me>
2022-08-02 16:02:59 -0500

    added random and audio

--- main.js
+++ main.js
@@ -6,16 +6,22 @@
 // 4. host to github pages
 
 ///////////////////////////////
+let data = [];
+let groupedData = [];
+
+// Questions
+// 1. Random question is * by 1000 now. Need to tie it to the array length.
+// 2.
 
 // info relating to jeopardy.json file
 
 let readJeopardyData = async () => {
   let rawJeopardyData = await fetch("jeopardy.json");
-  let data = await rawJeopardyData.json();
+  data = await rawJeopardyData.json();
   // console.log(data);
 
-  let groupedData = _.groupBy(data, "value");
-  console.log(groupedData.$200);
+  groupedData = _.groupBy(data, "value");
+  console.log(groupedData);
 };
 
 readJeopardyData();
@@ -39,13 +45,20 @@ document.body.appendChild(scoreLabel);
 scoreLabel.style.textAlign = "center";
 // score count
 let score = document.createElement("p");
-score.innerText = "0";
+let scoreCount = 0;
+score.innerText = scoreCount;
 // put score count under score label
 scoreLabel.appendChild(score);
 
-//////////////////////////////////////
+////////////////////
+//CREATE GRID
+////////////////////
 
-//create grid
+// grid variables
+let isClicked = false;
+
+let randomQuestion = Math.ceil(Math.random() * 1000);
+
 let grid = document.createElement("div");
 // create grid id
 grid.id = "grid";
@@ -54,7 +67,6 @@ document.body.appendChild(grid);
 // css for grid
 grid.style.display = "grid";
 grid.style.gridTemplateColumns = "auto auto auto auto auto";
-
 /////////////////////////////
 // add cells to grid container
 /////////////////////////////
@@ -66,16 +78,19 @@ let container = document.querySelector("#grid");
 // $100 cells
 //////////////////////
 
-
-
 function create100() {
   for (let i = 1; i <= 5; i++) {
     let cell100 = document.createElement("div");
     // set same class for all cells
-    cell100.className = 'cell'
+    cell100.className = "cell";
     cell100.innerHTML = "$100";
     container.appendChild(cell100);
-    
+
+    cell100.addEventListener("click", function () {
+      console.log(`click`);
+      isClicked = true;
+      question.innerText = groupedData.$100[randomQuestion].question;
+    });
   }
 }
 
@@ -89,9 +104,15 @@ function create200() {
   for (let i = 1; i <= 5; i++) {
     let cell200 = document.createElement("div");
     // set same class for all cells
-    cell200.className = 'cell'
+    cell200.className = "cell";
     cell200.innerHTML = "$200";
     container.appendChild(cell200);
+
+    cell200.addEventListener("click", function () {
+      console.log(`click`);
+      isClicked = true;
+      question.innerText = groupedData.$200[randomQuestion].question;
+    });
   }
 }
 
@@ -105,12 +126,17 @@ function create300() {
   for (let i = 1; i <= 5; i++) {
     let cell300 = document.createElement("div");
     // set same class for all cells
-    cell300.className = 'cell'
+    cell300.className = "cell";
     cell300.innerHTML = "$300";
     container.appendChild(cell300);
+
+    cell300.addEventListener("click", function () {
+      console.log(`click`);
+      isClicked = true;
+      question.innerText = groupedData.$300[randomQuestion].question;
+    });
   }
 }
-
 create300();
 
 //////////////////
@@ -121,9 +147,15 @@ function create400() {
   for (let i = 1; i <= 5; i++) {
     let cell400 = document.createElement("div");
     // set same class for all cells
-    cell400.className = 'cell'
+    cell400.className = "cell";
     cell400.innerHTML = "$400";
     container.appendChild(cell400);
+
+    cell400.addEventListener("click", function () {
+      console.log(`click`);
+      isClicked = true;
+      question.innerText = groupedData.$400[randomQuestion].question;
+    });
   }
 }
 
@@ -137,9 +169,15 @@ function create500() {
   for (let i = 1; i <= 5; i++) {
     let cell500 = document.createElement("div");
     // set same class for all cells
-    cell500.className = 'cell'
+    cell500.className = "cell";
     cell500.innerHTML = "$500";
     container.appendChild(cell500);
+
+    cell500.addEventListener("click", function () {
+      console.log(`click`);
+      isClicked = true;
+      question.innerText = groupedData.$500[randomQuestion].question;
+    });
   }
 }
 
@@ -148,46 +186,34 @@ create500();
 /////////////////////////////////////
 // place where user sees question prompt
 //////////////////////////////////////
-let section = document.createElement('section');
-let question = document.createElement('p')
-question.innerText = 'SELECT A BOX TO START'
-question.style.fontSize = '20px'
-question.style.textAlign = 'center'
-document.body.appendChild(section)
-section.appendChild(question)
-
-////////////////////////////////
-// create HTML break. 
-// Need spacing between grid, question, and answer
-/////////////////////////////////////
-// let br = document.createElement('br')
-// document.body.appendChild(br)
-
+let section = document.createElement("section");
+let question = document.createElement("p");
+question.innerText = "SELECT A BOX TO START";
+question.style.fontSize = "20px";
+question.style.textAlign = "center";
+document.body.appendChild(section);
+section.appendChild(question);
 
 /////////////////////////////////
 // place where user submits answer
 ///////////////////////////////////
 
 // create form. need input. need submit button.
-let form = document.createElement('form')
-form.id = 'form'
-document.body.appendChild(form)
-let input = document.createElement('input')
-input.placeholder = 'Enter your answer'
-input.type = 'text'
-input.id = 'input'
-input.style.width = '250px'
-input.style.height = '40px'
-form.appendChild(input)
-let button = document.createElement('button')
-button.type = 'submit'
-button.innerText = "SUBMIT"
-button.style.height = '46px'
-button.id = 'submit'
-form.appendChild(button)
+let form = document.createElement("form");
+form.id = "form";
+document.body.appendChild(form);
+let input = document.createElement("input");
+input.placeholder = "Enter your answer";
+input.type = "text";
+input.id = "input";
+input.style.width = "250px";
+input.style.height = "40px";
+form.appendChild(input);
+let button = document.createElement("button");
+button.type = "submit";
+button.innerText = "SUBMIT";
+button.style.height = "46px";
+button.id = "submit";
+form.appendChild(button);
 // css for form
-form.style.textAlign = 'center'
-
-
-
-
+form.style.textAlign = "center";
