8f804a7bc1ba815bfca42e2b23aa5dd97f2ea93a
John G <johngouldman@pm.me>
2022-08-03 16:26:03 -0500

    game logic updates

--- main.js
+++ main.js
@@ -8,10 +8,16 @@
 ///////////////////////////////
 let data = [];
 let groupedData = [];
+let q100 = [];
+let q200 = [];
+let q300 = [];
+let q400 = [];
+let q500 = [];
+let answer = "";
 
-// Questions
-// 1. Random question is * by 1000 now. Need to tie it to the array length.
-// 2.
+// Discuss
+// 1. toLowerCase(). Got it to work without it.
+// 2. other bugs.
 
 // info relating to jeopardy.json file
 
@@ -21,7 +27,13 @@ let readJeopardyData = async () => {
   // console.log(data);
 
   groupedData = _.groupBy(data, "value");
-  console.log(groupedData);
+  // console.log(groupedData);
+
+  q100 = groupedData.$100;
+  q200 = groupedData.$200;
+  q300 = groupedData.$300;
+  q400 = groupedData.$400;
+  q500 = groupedData.$500;
 };
 
 readJeopardyData();
@@ -40,9 +52,10 @@ title.style.marginRight = "auto";
 // create score label
 let scoreLabel = document.createElement("h3");
 scoreLabel.innerText = "YOUR SCORE:";
-scoreLabel.style.color = '#fe3e3d';
-scoreLabel.style.fontSize = '35px';
-scoreLabel.style.textShadow = '2px 1px black';
+scoreLabel.id = "scoreLabel";
+scoreLabel.style.color = "#00008b";
+scoreLabel.style.fontSize = "35px";
+// scoreLabel.style.textShadow = "1px 1px black";
 document.body.appendChild(scoreLabel);
 // score label css
 scoreLabel.style.textAlign = "center";
@@ -50,9 +63,9 @@ scoreLabel.style.textAlign = "center";
 let score = document.createElement("p");
 let scoreCount = 0;
 score.innerText = scoreCount;
-score.style.fontSize = '35px'
-score.style.margin = '15px'
-score.style.color = 'black';
+score.style.fontSize = "35px";
+score.style.margin = "15px";
+score.style.color = "black";
 // put score count under score label
 scoreLabel.appendChild(score);
 
@@ -63,7 +76,11 @@ scoreLabel.appendChild(score);
 // grid variables
 let isClicked = false;
 
-let randomQuestion = Math.ceil(Math.random() * 1000);
+let random1 = Math.ceil(Math.random() * 2009);
+let random2 = Math.ceil(Math.random() * 6890);
+let random3 = Math.ceil(Math.random() * 1947);
+let random4 = Math.ceil(Math.random() * 9610);
+let random5 = Math.ceil(Math.random() * 1996);
 
 let grid = document.createElement("div");
 // create grid id
@@ -84,11 +101,17 @@ let container = document.querySelector("#grid");
 // $100 cells
 //////////////////////
 
+let c100 = false;
+let c200 = false;
+let c300 = false;
+let c400 = false;
+let c500 = false;
+
 function create100() {
   for (let i = 1; i <= 5; i++) {
     let cell100 = document.createElement("div");
     // set same class for all cells
-    cell100.className = "cell";
+    cell100.className = `cell`;
     cell100.innerHTML = "$100";
     container.appendChild(cell100);
 
@@ -96,8 +119,17 @@ function create100() {
       event.preventDefault();
       console.log(`click`);
       isClicked = true;
-      console.log(groupedData.$100[randomQuestion]);
-      question.innerHTML = groupedData.$100[randomQuestion].question;
+      c100 = true;
+      // changes css of clicked cell
+      if ((isClicked = true)) {
+        cell100.style.backgroundColor = "black";
+      }
+
+      console.log(q100[random1]);
+      // This populates the random question and makes it visible to the user
+      question.innerHTML = q100[random1].question;
+      answer = q100[random1].answer;
+      console.log(` $100 answer: ${answer}`);
     });
   }
 }
@@ -119,7 +151,15 @@ function create200() {
     cell200.addEventListener("click", function () {
       console.log(`click`);
       isClicked = true;
-      question.innerHTML = groupedData.$200[randomQuestion].question;
+      c200 = true;
+      // changes css of clicked cell
+      if ((isClicked = true)) {
+        cell200.style.backgroundColor = "black";
+      }
+
+      question.innerHTML = q200[random2].question;
+      answer = q200[random2].answer;
+      console.log(` $200 answer: ${answer}`);
     });
   }
 }
@@ -141,7 +181,14 @@ function create300() {
     cell300.addEventListener("click", function () {
       console.log(`click`);
       isClicked = true;
-      question.innerHTML = groupedData.$300[randomQuestion].question;
+      c300 = true;
+      // changes css of clicked cell
+      if ((isClicked = true)) {
+        cell300.style.backgroundColor = "black";
+      }
+      question.innerHTML = q300[random3].question;
+      answer = q300[random3].answer;
+      console.log(` $300 answer: ${answer}`);
     });
   }
 }
@@ -162,7 +209,14 @@ function create400() {
     cell400.addEventListener("click", function () {
       console.log(`click`);
       isClicked = true;
-      question.innerHTML = groupedData.$400[randomQuestion].question;
+      c400 = true;
+      // changes css of clicked cell
+      if ((isClicked = true)) {
+        cell400.style.backgroundColor = "black";
+      }
+      question.innerHTML = q400[random4].question;
+      answer = q400[random4].answer;
+      console.log(` $400 answer: ${answer}`);
     });
   }
 }
@@ -184,7 +238,14 @@ function create500() {
     cell500.addEventListener("click", function () {
       console.log(`click`);
       isClicked = true;
-      question.innerHTML = groupedData.$500[randomQuestion].question;
+      c500 = true;
+      // changes css of clicked cell
+      if ((isClicked = true)) {
+        cell500.style.backgroundColor = "black";
+      }
+      question.innerHTML = q500[random5].question;
+      answer = q500[random5].answer;
+      console.log(` $500 answer: ${answer}`);
     });
   }
 }
@@ -197,29 +258,29 @@ create500();
 
 //create parent section
 let section = document.createElement("section");
-section.id = 'section'
+section.id = "section";
 document.body.appendChild(section);
 
 let section1 = document.createElement("section");
-section1.id = 'section1'
+section1.id = "section1";
 section.appendChild(section1);
 
 // dancing man gif
-let dancing = document.createElement('img');
-dancing.id = 'gif'
-dancing.src = "https://muz.usovairina.ru/wp-content/uploads/2020/05/transparent-dance-gif-5.gif"
-section1.appendChild(dancing)
+let dancing = document.createElement("img");
+dancing.id = "gif";
+dancing.src =
+  "https://muz.usovairina.ru/wp-content/uploads/2020/05/transparent-dance-gif-5.gif";
+section1.appendChild(dancing);
 
 // create child section
 let section2 = document.createElement("section");
-section2.id = 'section2'
+section2.id = "section2";
 section.appendChild(section2);
-
 
 let question = document.createElement("p");
 question.innerText = `WELCOME TO JEOPARDY! \n CLICK A CATEGORY TO GET STARTED!`;
 question.style.fontSize = "26px";
-question.style.fontWeight = 'bold'
+question.style.fontWeight = "bold";
 question.style.textAlign = "center";
 section2.appendChild(question);
 
@@ -233,35 +294,66 @@ div.id = "div";
 section2.appendChild(div);
 let input = document.createElement("input");
 input.placeholder = "Enter your answer";
-input.style.color = 'white'
-input.style.fontSize = '25px'
+input.style.color = "white";
+input.style.fontSize = "25px";
 input.type = "text";
 input.id = "input";
-input.style.width = "400px";
+input.style.width = "300px";
 input.style.height = "50px";
-input.style.backgroundColor = '#333333'
+input.style.backgroundColor = "#333333";
 div.appendChild(input);
 
 let button = document.createElement("button");
 button.innerText = "SUBMIT";
-button.style.height = '55px';
+button.style.height = "55px";
 button.id = "button";
-button.style.backgroundColor = '#fe3e3d'
+button.style.backgroundColor = "#fe3e3d";
 div.appendChild(button);
 // css for form
 div.style.textAlign = "center";
 
-//game logic
+/////////////////////////////
+// Answer submit / game logic
+////////////////////////////
 
-let guess = input.innerText;
-// This is the answer
-console.log(`Answer: ${groupedData.$100[randomQuestion].answer}`);
+let guess = input.value;
+// lets people answer correcly even if not capitalized
+let answer1 = answer.toLowerCase();
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function (event) {
+  event.preventDefault();
   console.log("click");
-  if (guess === groupedData.$100[randomQuestion].answer) {
-    question.innerText = `THAT'S RIGHT!`;
-  } else {
-    question.innerText = `You suck`;
+  if (input.value === answer) {
+    question.innerText = `You guessed right!`;
+    // adding score logic
+    if (c100 === true) {
+      scoreCount += 100;
+      score.innerText = scoreCount;
+      input.value = "";
+      c100 = false;
+    } else if (c200 === true) {
+      scoreCount += 200;
+      score.innerText = scoreCount;
+      input.value = "";
+      c200 = false;
+    } else if (c300 === true) {
+      scoreCount += 300;
+      score.innerText = scoreCount;
+      input.value = "";
+      c300 = false;
+    } else if (c400 === true) {
+      scoreCount += 400;
+      score.innerText = scoreCount;
+      input.value = "";
+      c400 = false;
+    } else if (c500 === true) {
+      scoreCount += 500;
+      score.innerText = scoreCount;
+      input.value = "";
+      c500 = false;
+    }
+  } else if (guess !== answer) {
+    question.innerText = `Sorry, maybe next time!`;
+    input.value = "";
   }
 });
